90fe6353c42122b3bc5ed653c5a2df4d8d4bba44
Dawei Wei <Dawei Wei>
2016-09-19 01:02:40 -0700

    Add emacs init.el

---
+++ init.el
@@ -0,0 +1,117 @@
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+
+(when (>= emacs-major-version 24)
+  (require 'package)
+  (package-initialize)
+  (add-to-list 'package-archives '("melpa" . "http://stable.melpa.org/packages/") t)
+  )
+
+(require 'cl)
+
+;; add whatever packages you want here
+(defvar davwei/packages '(
+			  company
+			  monokai-theme
+			  smex
+			  swiper
+			  counsel
+			  ) "Default packages")
+
+(setq package-selected-packages davwei/packages)
+
+(defun davwei/packages-installed-p()
+  (loop for pkg in davwei/packages
+	when (not (package-installed-p pkg)) do (return nil)
+	finally (return t)))
+
+(unless (davwei/packages-installed-p)
+  (message "%s" "Refreshing package database...")
+  (package-refresh-contents)
+  (dolist (pkg davwei/packages)
+    (when (not (package-installed-p pkg))
+      (package-install pkg))))
+
+;; config for smex
+(require 'smex) ; Not needed if you use package.el
+(smex-initialize) ; Can be omitted. This might cause a (minimal) delay
+                  ; when Smex is auto-initialized on its first run.
+;; binding some key
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+;; This is your old M-x.
+(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
+
+
+;; config for swiper
+(ivy-mode 1)
+(setq ivy-use-virtual-buffers t)
+(global-set-key "\C-s" 'swiper)
+(global-set-key (kbd "C-c C-r") 'ivy-resume)
+(global-set-key (kbd "M-x") 'counsel-M-x)
+(global-set-key (kbd "C-x C-f") 'counsel-find-file)
+(global-set-key (kbd "<f1> f") 'counsel-describe-function)
+(global-set-key (kbd "<f1> v") 'counsel-describe-variable)
+
+
+;; turn off tool bar
+(tool-bar-mode -1)
+
+;; turn off scroll bar
+(scroll-bar-mode -1)
+
+;; show line number
+(global-linum-mode t)
+
+;; turn off splash screen
+(setq inhibit-splash-screen t)
+
+;; open with full screen
+(setq initial-frame-alist (quote ((fullscreen . maximized))))
+
+;; show match parents
+(add-hook 'emacs-lisp-mode-hook 'show-paren-mode)
+
+;; highligh current line
+(global-hl-line-mode t)
+
+(load-theme 'monokai t)
+
+(defun open-my-init-file()
+  (interactive)
+  (find-file "~/.emacs.d/init.el"))
+
+(global-set-key (kbd "<f2>") 'open-my-init-file)
+
+(global-company-mode t)
+(setq cursor-type 'bar)
+(setq make-backup-files nil)
+
+(require 'org)
+(setq org-src-fontify-natively t)
+
+(require 'recentf)
+(recentf-mode t)
+(setq recentf-max-menu-items 25)
+(global-set-key "\C-x\ \C-r" 'recentf-open-files)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("a800120841da457aa2f86b98fb9fd8df8ba682cebde033d7dbf8077c1b7d677a" default)))
+)
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;; add delete section mode
+(delete-selection-mode t)
