a9cde6c2dce2d77fbbea0210a47a59c22c9ff2b4
Dawei Wei <Dawei Wei>
2016-09-23 23:55:58 -0700

    rebinding company mode M-n and M-p to C-n and C-p

--- init.el
+++ init.el
@@ -13,8 +13,8 @@
 (require 'init-packages)
 (require 'init-ui)
 (require 'init-better-defaults)
-(require 'init-keybindings)
 (require 'init-org)
+(require 'init-keybindings)
 
 (setq custom-file (expand-file-name "lisp/custom.el" user-emacs-directory))
 (load-file custom-file)
--- lisp/init-keybindings.el
+++ lisp/init-keybindings.el
@@ -36,4 +36,10 @@
 ;; r aks remember
 (global-set-key (kbd "C-c r") 'org-capture)
 
+(with-eval-after-load 'company
+  (define-key company-active-map (kbd "M-n") nil)
+  (define-key company-active-map (kbd "M-p") nil)
+  (define-key company-active-map (kbd "C-n") #'company-select-next)
+  (define-key company-active-map (kbd "C-p") #'company-select-previous))
+
 (provide 'init-keybindings)
